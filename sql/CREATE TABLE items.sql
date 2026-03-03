057889b4e9392a646426d267133ede18e8f2a2ed
Borys Ziukov <ziukoff1985@gmail.com>
2026-03-03 20:20:11 +0200

    Add tables: orders, fix tables: items

--- sql/CREATE TABLE items.sql
+++ sql/CREATE TABLE items.sql
@@ -5,7 +5,7 @@ CREATE TABLE items (
     brand_id INT NOT NULL,
     model_id INT NOT NULL,
     store_id INT NOT NULL,
-    price NUMERIC(10, 2) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL CHECK (price >= 0) DEFAULT 0.00,
     amount INT CHECK (amount >= 0),
     PRIMARY KEY (id),
     CONSTRAINT fk_items_item_categories FOREIGN KEY (category_id) REFERENCES item_categories ON DELETE CASCADE,
@@ -13,5 +13,5 @@ CREATE TABLE items (
     CONSTRAINT fk_items_brands FOREIGN KEY (brand_id) REFERENCES brands ON DELETE CASCADE,
     CONSTRAINT fk_items_models FOREIGN KEY (model_id) REFERENCES models ON DELETE CASCADE,
     CONSTRAINT fk_items_stores FOREIGN KEY (store_id) REFERENCES stores ON DELETE RESTRICT
-)
+);
 
