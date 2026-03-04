65c8392beffdb2814f6bfb3e8b7ecd47be70a18d
Borys Ziukov <ziukoff1985@gmail.com>
2026-03-04 13:50:24 +0200

    Fix CONSTRAINTs in orders, items

--- sql/CREATE TABLE orders.sql
+++ sql/CREATE TABLE orders.sql
@@ -2,7 +2,7 @@ CREATE TABLE orders (
     id SERIAL,
     code VARCHAR(20) NOT NULL UNIQUE,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    amount NUMERIC(10, 2) NOT NULL CONSTRAINT positive_amount CHECK (amount >= 0),
+    amount NUMERIC(10, 2) NOT NULL CONSTRAINT orders_amount CHECK (amount >= 0),
     paid BOOLEAN NOT NULL DEFAULT FALSE,
     customer_id INT NOT NULL,
     PRIMARY KEY (id),
